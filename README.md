0e0b1d2481c3679f151ec6c07551fc21ff0ac2a4
Jihyuk Lee <lead@sver.dev>
2025-07-13 02:13:08 +0900

    chore :: 잡다한 변경

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# DotFiles
+# Dotfiles
--- start.sh
+++ start.sh
@@ -9,6 +9,3 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 ln -nfs $HOME/dotfiles/Brewfile $HOME/Brewfile
 ln -nfs $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -nfs $HOME/dotfiles/.gitignore $HOME/.gitignore
-
-sudo sh -c "echo $(which nu) >> /etc/shells"
-chsh -s $(which nu)
